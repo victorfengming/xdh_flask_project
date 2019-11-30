13a1fb36e6731aeb75d5a4c4d622feb525a8f5ac
victor <fengming19981221@163.com>
2019-11-30 13:29:31 +0800

    添加的前台表单页面成了

--- web_blueprint/app/admin/views.py
+++ web_blueprint/app/admin/views.py
@@ -1,5 +1,5 @@
 from . import admin
-from flask import render_template,jsonify
+from flask import render_template, jsonify, request
 from app.models import Model
 # 后台首页
 @admin.route("/")
@@ -13,5 +13,15 @@ def books_index():
     res = Model().query('select * from user')
     print(res)
 
-    return jsonify(res)
-    # return render_template("book/index.html")
+    # return jsonify(res)
+    return render_template("book/index.html")
+
+# 图书的添加
+@admin.route('/books/add',methods=['GET','POST'])
+def books_add():
+    # 在这里一个 视图函数就都能搞定了
+    # 判断当前的请求方式
+    if request.method == 'GET':
+        # xianshixian显示添加的表单
+        return render_template("book/add.html")
+
--- web_blueprint/books.sql
+++ web_blueprint/books.sql
@@ -1,20 +1,16 @@
 create table wxapp.books
 (
-    id           int            not null comment '主键'
-        primary key,
+    id           int            not null comment '主键' primary key,
     title        varchar(255)   null comment '书名',
     author       varchar(255)   null comment '作者',
-    pic_url      varchar(255)   null comment '封面图
-',
+    pic_url      varchar(255)   null comment '封面图',
     publisher    varchar(255)   null comment '出版社',
     price        decimal(10, 2) null comment '价格',
     isbn13       varchar(255)   null comment '书号',
     summary      text           null comment '内容推荐',
-    catalog      text           null comment '目录
-',
+    catalog      text           null comment '目录',
     author_intro varchar(255)   null comment '作者介绍',
-    pubdate      varchar(255)   null comment '出版日期
-',
+    pubdate      varchar(255)   null comment '出版日期',
     status       varchar(255)   null comment '状态 0 ok 1 下架',
     num          int            null comment '可借数量',
     addtime      datetime       null comment '添加时间'
