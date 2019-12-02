bbb9bea52be09c22330509fe27aca818ec8c68ab
victor <fengming19981221@163.com>
2019-12-02 10:31:07 +0800

    微信app init

--- web_blueprint/app/api/views.py
+++ web_blueprint/app/api/views.py
@@ -1,7 +1,20 @@
+from flask import jsonify
+
 from . import api
+from app.models import Model
 
 
 #
 @api.route("/")
 def index():
     return "<h1 style='color:green'>this is home</h1>"
+
+#
+@api.route("/list")
+def books_list():
+    # 获取当前所有书籍信息,并返回json数据
+    data = Model().query('select * from wxapp.books')
+    print(data)
+
+    return jsonify(data)
+
