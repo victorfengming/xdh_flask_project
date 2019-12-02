9c2808b726bd9c9ccda9fe4ddb7a59575a3e0ac5
victor <fengming19981221@163.com>
2019-12-02 11:13:48 +0800

    修改了一点标题啥的

--- weixinapp/pages/logs/logs.js
+++ weixinapp/pages/logs/logs.js
@@ -5,11 +5,14 @@ Page({
   data: {
     logs: []
   },
+
+  // 这个是刚加载完执行的函数
   onLoad: function () {
+    console.log("哈哈");
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
         return util.formatTime(new Date(log))
       })
     })
   }
-})
+});
